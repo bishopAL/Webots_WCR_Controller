3494a146811641195c762ddc9a897f548aafc2c8
王炳诚 <wangbingcheng@MacBook-Pro.local>
2020-06-03 15:22:34 +0800

    update cpp controller for simulation

---
+++ webot_simulation/controllers/GeckoRunner/GeckoRunner.cpp
@@ -0,0 +1,71 @@
+// File:          GeckoRunner.cpp
+// Date:
+// Description:
+// Author:
+// Modifications:
+
+// You may need to add webots include files such as
+// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
+// and/or to add some other includes
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+const char* MOTION_NAMES[4][3] = {
+   {"RF0","RF1", "RF2"},
+   {"RH0","RH1", "RH2"},
+   {"LF0","LF1", "LF2"},
+   {"LH0", "LH1","LH2"}
+};
+Motor *limb_motors[4][3];
+// This is the main program of your controller.
+// It creates an instance of your Robot instance, launches its
+// function(s) and destroys it at the end of the execution.
+// Note that only one instance of Robot should be created in
+// a controller program.
+// The arguments of the main function can be specified by the
+// "controllerArgs" field of the Robot node
+int main(int argc, char **argv) {
+  // create the Robot instance.
+  Robot *robot = new Robot();
+  for(int i=0;i<4;i++)
+  {
+    for(int j=0;j<3;j++)
+    {
+      limb_motors[i][j] = robot->getMotor(MOTION_NAMES[i][j]);
+    }
+  }
+  for(int i=0;i<4;i++)
+  {
+    for(int j=0;j<3;j++)
+    {
+      limb_motors[i][j]->setPosition(0.0);
+    }
+  }
+  // get the time step of the current world.
+  int timeStep = (int)robot->getBasicTimeStep();
+
+  // You should insert a getDevice-like function in order to get the
+  // instance of a device of the robot. Something like:
+  //  Motor *motor = robot->getMotor("motorname");
+  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
+  //  ds->enable(timeStep);
+
+  // Main loop:
+  // - perform simulation steps until Webots is stopping the controller
+  while (robot->step(timeStep) != -1) {
+    // Read the sensors:
+    // Enter here functions to read sensor data, like:
+    //  double val = ds->getValue();
+
+    // Process sensor data here.
+
+    // Enter here functions to send actuator commands, like:
+    //  motor->setPosition(10.0);
+  };
+
+  // Enter here exit cleanup code.
+
+  delete robot;
+  return 0;
+}
---
+++ webot_simulation/controllers/GeckoRunner/build/release/GeckoRunner.d
@@ -0,0 +1,5 @@
+build/release/GeckoRunner.o: GeckoRunner.cpp \
+  /Applications/Webots.app/include/controller/cpp/webots/Robot.hpp \
+  /Applications/Webots.app/include/controller/cpp/webots/Motor.hpp \
+  /Applications/Webots.app/include/controller/cpp/webots/Device.hpp \
+  /Applications/Webots.app/include/controller/cpp/webots/../../c/webots/types.h
